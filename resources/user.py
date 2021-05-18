1f4ab6f59d2e08b1fee5b3280d1fa302f9c7100d
Philip Cara <philipcara@gmail.com>
2021-05-18 08:28:04 -0400

    added logout

--- resources/user.py
+++ resources/user.py
@@ -1,23 +1,33 @@
-import sqlite3
+from werkzeug.security import safe_str_cmp
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+    get_jwt
+)
+from blacklist import _blacklist
+# underscores to start represent private functions that shouldn't be imported
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank"
+                          )
+_user_parser.add_argument('password',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank"
+                          )
 
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank"
-                        )
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank"
-                        )
 
     def post(self):
-        req = UserRegister.parser.parse_args()
+        req = _user_parser.parse_args()
         user = UserModel.find_by_username(req['username'])
 
         if user:
@@ -26,3 +36,56 @@ class UserRegister(Resource):
             user = UserModel(**req)
             UserModel.register(user)
             return {'message': 'User Registered'}, 200
+
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': "User not found"}, 404
+        else:
+            return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': "User not found"}, 404
+        else:
+            user.remove_from_db()
+            return {'message': 'User Deleted'}, 200
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        data = _user_parser.parse_args()
+
+        user = UserModel.find_by_username(data['username'])
+
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                       'access_token': access_token,
+                       'refresh_token': refresh_token
+                   }, 200
+        return {'message': 'Invalid Credentials'}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()['jti']
+        _blacklist.append(jti)
+        return {'message': 'You are logged out'}, 200
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user, fresh=False)
+        return access_token
